ac723cfe5d5d64192373c4cf5a838132e673c923
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-04-20 16:22:10 +0700

    colors setup tailwindcss

--- app/(tabs)/(destinations)/index.tsx
+++ app/(tabs)/(destinations)/index.tsx
@@ -1,12 +1,12 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export default function index() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center bg-background">
       <Text>Default Text</Text>
-      <Text className="font-roboto-bold text-white">Roboto Text</Text>
+      <Text className="font-roboto-bold text-primary">Roboto Text</Text>
       <Text className="font-roboto-slab-semi-bold text-white">
         Roboto Slab Text
       </Text>
@@ -17,5 +17,3 @@ export default function index() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
