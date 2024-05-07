108442c3f1bc20a9cf3f3581e3c6ad574f48de84
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-05-07 23:25:50 +0700

    home places complete

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -5,17 +5,31 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { container } from "@/constants/Container";
 import { Text } from "react-native";
 import { text } from "@/constants/Text";
-import { Feather } from "@expo/vector-icons";
+import { Feather, FontAwesome5, FontAwesome6, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
 import { styles } from "@/app/(onboarding)";
 import AppIntroSlider from "react-native-app-intro-slider";
+import { Button, Card, Icon } from "@rneui/themed";
+import { PricingCard, lightColors } from "@rneui/base";
 
 export default function index() {
+  const [pagoda, setPagoda] = useState(false);
+  const [mountain, setMountain] = useState(false);
+  const [cultural, setCultural] = useState(false);
+  const [general, setGeneral] = useState(false);
+
+  const setAll = () => {
+    setPagoda(false);
+    setMountain(false);
+    setCultural(false);
+    setGeneral(false);
+  }
+
   return (
     <ScrollView style={container.scrollView}>
       <View style={container.root}>
@@ -33,6 +47,95 @@ export default function index() {
           </View>
           <View style={home.midContainer}>
             <SpecialOfferSlider />
+          </View>
+          
+          <View className="justify-start items-start inline-flex self-stretch gap-1.5 pr-1">
+          <Text className='pt-4 text-center text-base font-roboto-black'>Loại Tour</Text>
+          
+          <View className="flex-0  flex-row content-between justify-between self-stretch">
+            <TouchableOpacity 
+            className="flex-0 h-[76px] w-[76px] bg-brown rounded-xl items-center justify-center"
+            onPress={() => {setAll(); setPagoda(true);}}
+            >
+            <MaterialIcons name="temple-buddhist" size={46} color={pagoda?"white":"black"} />
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-0 h-[76px] w-[76px] bg-brown rounded-xl items-center justify-center"
+             onPress={() => {setAll(); setMountain(true);}}
+            >
+            <FontAwesome6 name="mountain-sun" size={30} color={mountain?"white":'black'} />
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-0 h-[76px] w-[76px] bg-brown rounded-xl items-center justify-center"
+             onPress={() => {setAll(); setCultural(true);}}
+            >
+            <MaterialCommunityIcons name="account-music" size={46} color={cultural?"white":'black'} />
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-0 h-[76px] w-[76px] bg-brown rounded-xl items-center justify-center"
+             onPress={() => {setAll(); setGeneral(true);}}
+            >
+            <FontAwesome5 name="map-marked-alt" size={34} color={general?"white":'black'} />
+            </TouchableOpacity>
+          </View>
+
+          <Text className='pt-4 text-center text-base font-roboto-black'>Tour Bán Chạy</Text>
+
+          <View className="flex-0 bg-milk flex-row self-stretch justify-between content-between p-2.5 rounded-2xl">
+            <View className="flex-1 justify-between py-1.5">
+              <Text className="text-sm font-roboto-condensed-black font-bold leading-none">Chùa Hang - Tuyệt sắc linh thiên Châu Đốc</Text>
+              <Text className="text-teal-500 text-sm font-roboto-black font-semibold leading-4">30/100</Text>
+              <Text className="text-black text-sm font-roboto font-normal leading-4">10K+</Text>
+              <Text className="text-rose-600 text-sm font-roboto font-normal leading-4">Từ 580.000 VND</Text>
+              <View className="flex-0 flex-row justify-between">
+                <Button
+                  title={"⭐ 4.5"}
+                  buttonStyle={{
+                    borderRadius: 13,
+                    backgroundColor: Colors.dark.black,
+                  }}
+                />
+                <Button
+                title={"Đặt Tour Ngay"}
+                buttonStyle={{
+                  borderRadius: 13,
+                  backgroundColor: Colors.dark.black,
+                }}
+              />
+              </View>
+            </View>
+            <View className="flex-0 items-end justify-center px-1.5">
+              <Image source={require('@/assets/images/chuahang.png')}/>
+            </View>
+          </View>
+
+          <Text className='pt-4 text-center text-base font-roboto-black'>Tour Mới Nhất</Text>
+
+          <View className="flex-0 bg-milk flex-row self-stretch justify-between content-between p-2.5 rounded-2xl">
+            <View className="flex-1 justify-between py-1.5">
+              <Text className="text-sm font-roboto-condensed-black font-bold leading-none">Chùa Hang - Tuyệt sắc linh thiên Châu Đốc</Text>
+              <Text className="text-teal-500 text-sm font-roboto-black font-semibold leading-4">30/100</Text>
+              <Text className="text-black text-sm font-roboto font-normal leading-4">10K+</Text>
+              <Text className="text-rose-600 text-sm font-roboto font-normal leading-4">Từ 580.000 VND</Text>
+              <View className="flex-0 flex-row justify-between">
+                <Button
+                  title={"⭐ 4.5"}
+                  buttonStyle={{
+                    borderRadius: 13,
+                    backgroundColor: Colors.dark.black,
+                  }}
+                />
+                <Button
+                title={"Đặt Tour Ngay"}
+                buttonStyle={{
+                  borderRadius: 13,
+                  backgroundColor: Colors.dark.black,
+                }}
+              />
+              </View>
+            </View>
+            <View className="flex-0 items-end justify-center px-1.5">
+              <Image source={require('@/assets/images/chuahang.png')}/>
+            </View>
+          </View>
+
           </View>
         </View>
       </View>
@@ -79,7 +182,7 @@ export const homeText = StyleSheet.create({
     color: Colors.primary,
   },
 });
-
+// SpecialOfferSlider
 const data = [
   {
     key: "1",
