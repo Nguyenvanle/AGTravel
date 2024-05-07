6f928a3460f720cf86d19f5ec666755efbca2011
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-05-07 11:17:54 +0700

    login fix services

--- app/(login)/LoginScreen.tsx
+++ app/(login)/LoginScreen.tsx
@@ -12,6 +12,7 @@ import Colors from "@/constants/Colors";
 import { useState } from "react";
 import { ButtonCustom } from "@/components/Button";
 import { signIn } from "@/services/firebaseAuth";
+import { replaceLog } from "@/services/routerLog";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -25,6 +26,7 @@ export default function LoginPage() {
       const userCredential = await signIn({ email, password });
       // Tiếp tục logic ở đây sau khi đăng nhập thành công
       console.log("Đăng nhập thành công:", userCredential);
+      replaceLog("/(tabs)/(home)", "(login) --> (home)");
     } catch (error: any) {
       // Hiển thị một thông báo lỗi nếu có
       console.log("Đăng nhập thất bại:", error);
@@ -61,6 +63,9 @@ export default function LoginPage() {
             textColor={Colors.light.milk}
           />
         )}
+        <Text className="text-white text-base font-roboto-bold">
+          Quên mật khẩu?
+        </Text>
       </View>
     </ScrollView>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,23 @@
 import Colors from "@/constants/Colors";
 import { container } from "@/constants/Container";
 import { useAuth } from "@/hooks/useAuth";
-import { Redirect, router, useSegments } from "expo-router";
-import { useEffect, useState } from "react";
+import { replaceLog } from "@/services/routerLog";
+import { router, useSegments } from "expo-router";
+import { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
 
 export default function RedirectPage() {
   // Hàm xác định trang hiện tại từ đó chuyển hướng tới trang tiếp theo dựa trên trang hiện tại
   const [currentSegments] = useSegments();
   const user = useAuth();
-  const [loading, setLoading] = useState(false);
-
+  // Hàm tải dữ liệu user và khi segments hiện tại được tải sau đó logic chuyển hướng
   useEffect(() => {
-    setLoading(true);
     if (user) {
       console.log(`/ x-> (home)`);
       router.replace(`/(tabs)/(home)`); // Chuyển đổi này phụ thuộc vào định nghĩa của các routes của bạn.
+    } else {
+      replaceLog("/(launch)", "/ --> (launch)");
     }
-    setLoading(false);
   }, [currentSegments, user]);
 
   return (
